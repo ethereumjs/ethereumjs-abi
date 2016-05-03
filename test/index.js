2c32388fd7e8ecccc3e94cc5f30ef32a856a0b8c
Alex Beregszaszi <alex@rtfs.hu>
2016-05-03 18:41:58 +0100

    Properly decode fixed-length arrays
    Fixes #13

--- test/index.js
+++ test/index.js
@@ -277,6 +277,18 @@ describe('decoding uint256[], bool', function () {
   })
 })
 
+describe('decoding fixed-array', function () {
+  it('uint[3]', function () {
+    var a = abi.rawDecode([ 'uint[3]' ], new Buffer('000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003', 'hex'))
+    console.log(a);
+    assert.equal(a.length, 1);
+    assert.equal(a[0].length, 3);
+    assert.equal(a[0][0].toString(10), 1);
+    assert.equal(a[0][1].toString(10), 2);
+    assert.equal(a[0][2].toString(10), 3);
+  })
+})
+
 /* FIXME: should check that the whole input buffer was consumed
 describe('decoding uint[2] with [1,2,3]', function () {
   it('should fail', function () {
